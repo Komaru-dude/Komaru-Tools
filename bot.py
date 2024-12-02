f8f9c6f36f8df2f5414904d154a4a0311f394e3b
Komaru-dude <honkhonkovic842@gmail.com>
2024-12-02 19:48:37 +0700

    Фикс

--- bot.py
+++ bot.py
@@ -2,25 +2,43 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from aiogram.enums import ParseMode
 from dotenv import load_dotenv
 import os
+
+# Загружаем переменные из dotenv
+load_dotenv()
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Токен бота
-pre_token = os.getenv('BOT_API_TOKEN')
-token = str(pre_token)
+token = os.getenv("BOT_API_TOKEN")
 # Объект бота
-bot = Bot(token)
+bot = Bot(token, ParseMode=ParseMode.MARKDOWN_V2)
 # Диспетчер
 dp = Dispatcher()
-# Загружаем переменные из dotenv
-load_dotenv()
 
-# Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("iдi нахуй")
+
+@dp.message(Command("info"))
+async def cmd_info(message: types.Message):
+    # Достаём информацию о пользователе
+    user = message.from_user
+    first_name = user.first_name
+    last_name = f" {user.last_name}" if user.last_name else ""
+    user_id = user.id
+    user_name = first_name + last_name
+    
+    # Ссылка на профиль по ID
+    profile_link = f"https://t.me/{user_id}"
+
+    # Формируем кликабельное имя пользователя
+    clickable_name = f"<a href='{profile_link}'>{user_name}</a>"
+    
+    # Отправляем сообщение с кликабельным именем и ссылкой на профиль по ID
+    await message.reply(f"Информация о пользователе {clickable_name}", parse_mode=ParseMode.HTML)
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
