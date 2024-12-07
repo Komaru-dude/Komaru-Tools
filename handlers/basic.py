08e59ebc08090d3326ee7bf6c85e26e3438aae9f
Komaru-dude <honkhonkovic842@gmail.com>
2024-12-07 22:02:49 +0700

    Update basic.py

--- handlers/basic.py
+++ handlers/basic.py
@@ -1,9 +1,10 @@
-import db
+import db, time
 from aiogram import types, Router
 from aiogram.types import FSInputFile, Message
 from aiogram.filters import Command
 
 base_router = Router()
+start_time = time.time()
 
 @base_router.message(Command("rules"))
 async def cmd_rules(message: Message):
@@ -33,3 +34,21 @@ async def cmd_start(message: types.Message):
     if not db.user_have_username(user_id):
         db.add_username(user_id, username)
     await message.reply(f"Гойда @{username}")
+
+@base_router.message(Command("status"))
+async def cmd_status(message: types.Message):
+    ping_start_time = time.monotonic()
+    sent_message = await message.reply("⏳")
+    end_time = time.monotonic()
+    ping = (end_time - ping_start_time) * 1000  # В миллисекундах
+    current_time = time.time()
+    uptime_seconds = int(current_time - start_time)
+
+    # Форматируем аптайм
+    days = uptime_seconds // 86400
+    hours = (uptime_seconds % 86400) // 3600
+    minutes = (uptime_seconds % 3600) // 60
+    seconds = uptime_seconds % 60
+
+    uptime_str = f"{days}д {hours}ч {minutes}м {seconds}с"
+    await sent_message.edit_text(f"Пинг: {int(ping)} мс\nБот работает: {uptime_str}")
