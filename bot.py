78eeeb441af6fd07a216ca70b65ab594cbf0b035
Komaru-dude <honkhonkovic842@gmail.com>
2024-12-07 22:22:11 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,22 +7,22 @@ from handlers.rights import rght_router
 from handlers.basic import base_router
 from handlers.text import txt_router
 
-# Загружаем переменные из dotenv
-load_dotenv()
-
-# Включаем логирование, чтобы не пропустить важные сообщения
-logging.basicConfig(level=logging.INFO)
-# Токен бота
-token = os.getenv("BOT_API_TOKEN")
-# Объект бота
-bot = Bot(token, ParseMode=ParseMode.MARKDOWN_V2)
-# Диспетчер
-dp = Dispatcher()
-
 # Запуск процесса поллинга новых апдейтов
 async def main():
+    # Загружаем переменные из dotenv
+    load_dotenv()
+    # Включаем логирование
+    logging.basicConfig(level=logging.INFO)
+    # Токен бота
+    token = os.getenv("BOT_API_TOKEN")
+    # Объект бота
+    bot = Bot(token, ParseMode=ParseMode.MARKDOWN_V2)
+    # Диспетчер
+    dp = Dispatcher()
+    # Включаем роутеры
     dp.include_routers(mod_router, rght_router, base_router)
     dp.include_router(txt_router)
+    # Наконец, запуск
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
