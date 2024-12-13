e6f32271a4a5147190bb5dd5f666090c43fdd321
Komaru-dude <honkhonkovic842@gmail.com>
2024-12-13 23:50:23 +0700

    Update basic.py

--- handlers/basic.py
+++ handlers/basic.py
@@ -92,7 +92,7 @@ async def cmd_info(message: types.Message):
         user_id = message.reply_to_message.from_user.id
         user = message.reply_to_message.from_user
         first_name = message.reply_to_message.from_user.first_name
-    elif "@" in parts1:
+    elif parts1 and "@" in parts1:
         mention_match = re.search(r"@(\w+)", message.text)
         username = mention_match.group(1)
         user_id = db.get_user_id_by_username(username=username)
