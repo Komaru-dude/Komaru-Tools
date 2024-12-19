cff9a532456aee31dfd4a12e25489bea549e079e
Komaru-dude <honkhonkovic842@gmail.com>
2024-12-19 21:59:53 +0700

    Минорные фиксы и дополнения

--- bot/__init__.py
+++ bot/__init__.py
@@ -4,10 +4,14 @@ from dotenv import load_dotenv
 # Загрузка переменных из .env
 load_dotenv()
 
+# Переменные окружения
 API_TOKEN = os.getenv('BOT_API_TOKEN')
 ADMIN_ID = os.getenv('ADMIN_ID')
 
+# Проверка обязательных переменных
 if not API_TOKEN:
     raise ValueError("API_TOKEN не задан в .env файле")
 if not ADMIN_ID:
     raise ValueError("ADMIN_ID не задан в .env файле")
+if not ADMIN_ID.isdigit():
+    raise ValueError("ADMIN_ID задан некорректно")
