1567cbc8fa4f83db49243cf15bfa9184adaba3c5
Komaru-dude <honkhonkovic842@gmail.com>
2024-12-29 00:44:10 +0700

    Update basic.py

--- bot/handlers/basic.py
+++ bot/handlers/basic.py
@@ -121,6 +121,7 @@ async def cmd_info(message: types.Message):
 
     # Получаем данные из db
     user_data = db.get_user_data(user_id)
+    msg_to_rep_up = user_data[8] - user_data[13]
     if not user_id == user_data[0]:
         db.update_user_id(user_data[0], user_id)
     # Формируем текст с информацией о пользователе
@@ -131,6 +132,7 @@ async def cmd_info(message: types.Message):
         f"🏅 Ранг: {user_data[6]}\n"
         f"💬 Кол-во сообщений: {user_data[8]}\n"
         f"💎 Репутация: {user_data[5]}\n"
+        f"🌀 Сообщений до повышения репутации: {msg_to_rep_up}"
         f"🖼 Демотиваторы: {user_data[9]}\n"
         f"🏷️ Префикс: {user_data[7]}"
     )
