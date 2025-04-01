9f16449b3b5c4deec166547145cb34221f291b70
Komaru-dude <honkhonkovic842@gmail.com>
2025-04-01 23:55:21 +0700

    Удалены лишние комментарии, заготовки для будущих изменений

--- bot/db.py
+++ bot/db.py
@@ -2,12 +2,20 @@ import sqlite3, os, json, time, random
 
 DB_PATH = 'users.db'
 
+RANK_TO_LEVEL = {
+    "Забанен": 0,
+    "Замьючен": 0,
+    "Участник": 1,
+    "Модератор": 2,
+    "Администратор": 3,
+    "Владелец": 4
+}
+
 def create_db():
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS users (
                         user_id INTEGER PRIMARY KEY,
-                        username INTEGER DEFAULT '',
                         warns INTEGER DEFAULT 0,
                         bans INTEGER DEFAULT 0,
                         mutes INTEGER DEFAULT 0,
@@ -24,26 +32,14 @@ def create_db():
     conn.commit()
     conn.close()
 
-# Проверяем существование базы данных и создание её при необходимости
 if not os.path.exists(DB_PATH):
     create_db()
 
-# Функция для проверки ранга пользователя
 def has_permission(user_id, level):
-    # Словарь с уровнями и соответствующими рангами
-    rank_to_level = {
-        "Забанен": 0,
-        "Замьючен": 0,
-        "Участник": 1,
-        "Модератор": 2,
-        "Администратор": 3,
-        "Владелец": 4
-    }
 
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     
-    # Извлекаем ранг пользователя
     cursor.execute('''SELECT rank FROM users WHERE user_id = ?''', (user_id,))
     result = cursor.fetchone()
     conn.close()
@@ -51,47 +47,13 @@ def has_permission(user_id, level):
     if result is None:
         return False
     
-    user_rank = result[0]  # Получаем статус пользователя
-    user_level = rank_to_level.get(user_rank)
+    user_rank = result[0]
+    user_level = RANK_TO_LEVEL.get(user_rank)
     
     if user_level is None:
-        return False  # Если ранг не найден в словаре, возвращаем False
-
-    return user_level >= level  # Проверяем, соответствует ли уровень пользователя требуемому
-
-def user_have_username(user_id):
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute('SELECT username FROM users WHERE user_id = ?', (user_id,))
-    exists = cursor.fetchone()
-    conn.close()
-    if not exists == '':
         return False
-    else:
-        return True
-    
-def add_username(user_id, username):
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute('UPDATE users SET username = ? WHERE user_id = ?', (username, user_id))
-    conn.commit()
-    conn.close()
 
-def get_username(user_id):
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute('''SELECT username FROM users WHERE user_id = ?''', (user_id,))
-    result = cursor.fetchone()
-    conn.close()
-    return result
-
-def get_user_id_by_username(username):
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute('''SELECT user_id FROM users WHERE username = ?''', (username,))
-    result = cursor.fetchone()
-    conn.close()
-    return result[0] if result else None
+    return user_level >= level
 
 def set_rank(user_id, rank):
     conn = sqlite3.connect(DB_PATH)
@@ -136,30 +98,26 @@ def update_user_warns(user_id, reason):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
-    # Получаем текущую историю и количество предупреждений
     cursor.execute('''SELECT history, warns FROM users WHERE user_id = ?''', (user_id,))
     result = cursor.fetchone()
 
     if result is None:
         history = []
-        warns = 0  # Если пользователь не существует, начинаем с 0 предупреждений
+        warns = 0
     else:
-        history = json.loads(result[0]) if result[0] else []  # Парсим историю, если она есть
-        warns = result[1]  # Получаем количество предупреждений
+        history = json.loads(result[0]) if result[0] else []
+        warns = result[1]
 
-    # Добавляем новое предупреждение
     punishment = {
         "type": "warn",
         "reason": reason,
-        "timestamp": int(time.time()),  # Время наказания в формате Unix timestamp
+        "timestamp": int(time.time()),
     }
 
     history.append(punishment)
 
-    # Обновляем только историю
     cursor.execute('''UPDATE users SET history = ? WHERE user_id = ?''', (json.dumps(history), user_id))
 
-    # Увеличиваем количество предупреждений
     warns += 1
     cursor.execute('''UPDATE users SET warns = ? WHERE user_id = ?''', (warns, user_id))
 
@@ -170,31 +128,27 @@ def update_user_bans(user_id, reason):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
-    # Получаем текущую историю
     cursor.execute('''SELECT history, bans FROM users WHERE user_id = ?''', (user_id,))
     result = cursor.fetchone()
 
     if result is None or not result[0]:
-        history = []  # Если истории нет, создаем новый список
+        history = []
         bans = 0
     else:
-        history = json.loads(result[0])  # Парсим историю как JSON
+        history = json.loads(result[0])
         bans = result[1]
 
-    # Добавляем наказание типа ban
     punishment = {
         "type": "ban",
-        "reason": reason,  # Причина передается как аргумент
-        "timestamp": int(time.time()),  # Время начала наказания в формате Unix timestamp
+        "reason": reason,
+        "timestamp": int(time.time()),
     }
 
     history.append(punishment)
 
-    # Увеличиваем количество банов
     bans += 1
     cursor.execute('''UPDATE users SET bans = ? WHERE user_id = ?''', (bans, user_id))
 
-    # Обновляем историю в базе данных
     cursor.execute('''UPDATE users SET history = ? WHERE user_id = ?''', (json.dumps(history), user_id))
     conn.commit()
     conn.close()
@@ -203,31 +157,27 @@ def update_user_mutes(user_id, reason):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
-    # Получаем текущую историю
     cursor.execute('''SELECT history, mutes FROM users WHERE user_id = ?''', (user_id,))
     result = cursor.fetchone()
 
     if result is None or not result[0]:
-        history = []  # Если истории нет, создаем новый список
+        history = []
         mutes = 0
     else:
-        history = json.loads(result[0])  # Парсим историю как JSON
+        history = json.loads(result[0])
         mutes = result[1]
 
-    # Добавляем наказание типа mute
     punishment = {
         "type": "mute",
-        "reason": reason,  # Причина передается как аргумент
-        "timestamp": int(time.time()),  # Время начала наказания в формате Unix timestamp
+        "reason": reason,
+        "timestamp": int(time.time()),
     }
 
-    # Увеличиваем количество мутов
     mutes += 1
     cursor.execute('''UPDATE users SET mutes = ? WHERE user_id = ?''', (mutes, user_id))
 
     history.append(punishment)
 
-    # Обновляем историю в базе данных
     cursor.execute('''UPDATE users SET history = ? WHERE user_id = ?''', (json.dumps(history), user_id))
     conn.commit()
     conn.close()
@@ -243,14 +193,11 @@ def get_user_data(user_id):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     
-    # Проверяем, существует ли пользователь
     cursor.execute('''SELECT * FROM users WHERE user_id = ?''', (user_id,))
     user_data = cursor.fetchone()
     
     if user_data is None:
-        # Если пользователя нет, создаём его
         add_user(user_id)
-        # Повторно извлекаем данные для нового пользователя
         cursor.execute('''SELECT * FROM users WHERE user_id = ?''', (user_id,))
         user_data = cursor.fetchone()
     
@@ -269,7 +216,6 @@ def get_history(user_id):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     
-    # Извлекаем историю наказания
     cursor.execute('''SELECT history FROM users WHERE user_id = ?''', (user_id,))
     result = cursor.fetchone()
     conn.close()
