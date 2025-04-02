fa030b01b656a4c56b5b6dfaa9132afcf376b32d
Komaru-dude <honkhonkovic842@gmail.com>
2025-04-02 13:12:36 +0700

    Крайне костыльный способ запуска двух ботов одновременно

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,32 +1,43 @@
-import asyncio, logging, os
+import asyncio, logging, os, threading
 from aiogram import Bot, Dispatcher
+from pyrogram import Client
 from dotenv import load_dotenv
 from .handlers.moderations import mod_router
 from .handlers.rights import rght_router
 from .handlers.basic import base_router
 from .handlers.text import txt_router
 
-async def main():
+load_dotenv()
+token = os.getenv("BOT_API_TOKEN")
+
+async def aio_main():
     logging.basicConfig(level=logging.INFO)
-    
-    # Инициализация бота
-    load_dotenv()
-    token = os.getenv("BOT_API_TOKEN")
     bot = Bot(token)
     dp = Dispatcher()
-    
-    # Регистрация роутеров
-    dp.include_routers(
-        mod_router,
-        rght_router,
-        base_router,
-        txt_router
-    )
-    
+    dp.include_routers(mod_router, rght_router, base_router, txt_router)
     try:
         await dp.start_polling(bot)
     finally:
         await bot.close()
+
+def run_aio():
+    asyncio.run(aio_main())
+
+async def pyro_main():
+    api_id = os.getenv("API_ID")
+    api_hash = os.getenv("API_HASH")
+    if not os.path.exists("my_bot.session"):
+        app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=token)
+    else:
+        app = Client("my_bot")
+    await app.start()
+    await asyncio.Event().wait()
+    await app.stop()
+
+async def main():
+    aio_thread = threading.Thread(target=run_aio, daemon=True)
+    aio_thread.start()
+    await pyro_main()
 
 if __name__ == "__main__":
     try:
